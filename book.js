eb75dfd241423cb4910925de279d6bb5631468e9
amanuelMecha <69689820+amanuelMecha@users.noreply.github.com>
2021-02-02 10:51:21 -0600

    index changed

--- book.js
+++ book.js
@@ -1,31 +1,22 @@
 
 window.onload = function () {
     const display = document.querySelector('#outlet');
-
     bookPage();
     function bookPage() {
-        // display.innerHTML = loginTemplate;
-        // const loginB = document.querySelector('#login');
         fetchLibraryApi()
     }
-
-    /*Animation page contains
-    -welcome text and user location , textbox for animation, refresh button and logout button
-     */
     async function fetchLibraryApi() {
         const response = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list", {
             method: 'GET'
         })
         const respondBody = await response.json();
         console.log(respondBody);
-        //var tbl = js.CreateTable(respondBody)
-        //display.innerHTML = tbl;
         buildTable(respondBody)
         function buildTable(data) {
             var table = document.getElementById("myTable")
             for (var i = 0; i < data.length; i++) {
                 var row = `<tr>
-                                    <td>${data[i].bookId}</td>
+                                    <td>${i + 1}</td>
                                     <td>${data[i].datePublished}</td>
                                     <td>${data[i].isbn}</td>
                                     <td>${data[i].overdueFee}</td>
@@ -34,13 +25,10 @@ window.onload = function () {
                                    </tr>`
                 table.innerHTML += row
             }
-
+            //data[i].bookId
         }
 
 
     }
     var addBook = document.getElementById("add")
-    //addBook.onclick(<li><a href="addBook.html"></a></li>)
-
-
 };
